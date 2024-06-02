ef9b32fdeda1c54aac74bdf96dd3a9c5ee70096d
al-amin90 <ijesun30@gmail.com>
2024-06-02 18:57:00 +0600

    verify admin, access admin dashboard user api creaed

--- index.js
+++ index.js
@@ -40,6 +40,16 @@ const verifyToken = (req, res, next) => {
   });
 };
 
+// make and send token
+app.post("/jwt", (req, res) => {
+  const userEmail = req.body;
+  console.log(userEmail);
+  const token = jwt.sign({ data: userEmail }, process.env.Token_Secret, {
+    expiresIn: "365d",
+  });
+  res.send({ token });
+});
+
 const db = client.db("nova-news");
 async function run() {
   try {
@@ -49,17 +59,19 @@ async function run() {
     const userCollection = db.collection("users");
     const articleCollection = db.collection("articles");
 
-    // make and send token
-    app.post("/jwt", (req, res) => {
-      const userEmail = req.body;
-      console.log(userEmail);
-      const token = jwt.sign({ data: userEmail }, process.env.Token_Secret, {
-        expiresIn: "365d",
-      });
-      res.send({ token });
-    });
+    // verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.data.email;
 
-    // save users
+      const result = await userCollection.findOne({ email });
+      console.log("working on verify", email, result?.isAdmin);
+      if (!result || !result?.isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    // save users in the db after login and singup
     app.post("/users", async (req, res) => {
       const currentUser = req.body;
       const email = req.body.email;
@@ -73,6 +85,39 @@ async function run() {
         res.send(result);
       }
     });
+
+    // ----------- admin related apis ------------
+
+    // get all users
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // make user admin
+    app.patch("/users/:email", verifyToken, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const isAdmin = req.body;
+      console.log(isAdmin, email);
+
+      const filter = { email: email };
+      const updateDoc = {
+        $set: {
+          ...isAdmin,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // is he/she admin
+    app.get("/user/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email });
+      res.send(user);
+    });
+
+    // --------- article related api -----------
 
     // add article in the db
     app.post("/article", verifyToken, async (req, res) => {
